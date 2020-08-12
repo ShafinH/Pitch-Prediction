49128632db8a69ce714107ad9cb042869b8d6b35
stenhaug <benastenhaug@gmail.com>
2020-08-12 13:59:42 -0700

    new code

--- mergeData.py
+++ mergeData.py
@@ -16,6 +16,6 @@ merged_data = pitches.merge(atbats, how = 'left', left_on = 'ab_id', right_on = 
 
 merged_data.rename(cloumns={'event': 'at_bat_outcome'})
 
-merged_data.to_csv('/Users/shafin/Documents/GitHub/Pitch/data-clean/test_clean.csv') 
+merged_data.to_csv('/Users/shafin/Documents/GitHub/Pitch/data-clean/test_clean.csv')
 
 print(merged_data.head())
