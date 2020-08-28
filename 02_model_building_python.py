a52a61a4be58426bab0a8518ff8bde03b479810c
ShafinH <shafin1025@gmail.com>
2020-08-28 08:25:19 -0700

    update

---
+++ 02_model_building_python.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+
+df_final = pd.read_csv("data-for-models/df_final.csv")
