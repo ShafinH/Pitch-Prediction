1c03f350a88498a4ba0fa7175e3b393df05d0d74
ShafinH <shafin1025@gmail.com>
2020-08-29 11:48:14 -0700

    cleaning

--- 00_mergeData.py
+++ 00_mergeData.py
@@ -10,12 +10,8 @@ player_names = pd.read_csv("data-raw/player_names.csv",
                            keep_default_na=False, na_values=[""])
 
 
-# print(atbats.head())
-
-merged_data = pitches.merge(atbats, how = 'left', left_on = 'ab_id', right_on = 'ab_id')
+merged_data = pitches.merge(atbats, how = 'left', left_on = 'ab_id', right_on = 'ab_id') # merging the atbats and pitches files
 
 merged_data.rename(cloumns={'event': 'at_bat_outcome'})
 
-merged_data.to_csv('/Users/shafin/Documents/GitHub/Pitch/data-clean/test_clean.csv')
-
-print(merged_data.head())
+merged_data.to_csv('/Users/shafin/Documents/GitHub/Pitch/data-clean/test_clean.csv') # creating csv file with the dataframe
