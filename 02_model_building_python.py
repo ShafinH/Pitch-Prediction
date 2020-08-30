79cd1ebd9ddf897620372f30d8b1ea4304eb5a15
ShafinH <65793752+ShafinH@users.noreply.github.com>
2020-08-29 20:59:48 -0700

    switching to nb

--- 02_model_building_python.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.datasets import make_blobs
-from sklearn.model_selection import train_test_split
-import numpy as np
-import matplotlib.pyplot as plt
-
-df_final = pd.read_csv("data-for-models/df_final.csv")
-
-df_final.head()
