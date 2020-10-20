a233e4a2f381e8bc6ee55af0a3806909c0e524b5
ShafinH <shafin1025@gmail.com>
2020-10-20 16:48:42 -0700

    starting shiny app

---
+++ shiny/server.R
@@ -0,0 +1,48 @@
+library(shiny)
+library(tidyverse)
+
+data <- 
+  tibble()
+
+# server logic
+server <- function(input, output) {
+  
+  getResults <- eventReactive(input$go, {
+    tibble(
+      s_count = input$s_count,
+      b_count = input$b_count,
+      pitch_num = input$s_count + input$b_count + 1,
+      last_ff_not = input$last_ff_not,
+      on_1b = input$on_1b,
+      on_2b = input$on_2b,
+      on_3b = input$on_3b,
+      stand = input$stand,
+      p_throws = input$p_throws,
+      pitcher_id = input$pitcher_id
+    )
+  })
+  
+  
+  output$table <- renderTable({
+    results <- getResults()
+    
+    # we need to write to make prediction on results, output on table below
+    # save the model to a file
+    # load svm in server
+    # use predict function on results
+    
+    # model <- read_rds(“~/Desktop/model.rds”)
+
+    
+    prediction <- predict(model,results)
+    
+    tibble(
+      `_` = c("Note", "Probability"),
+      `__` = c("The output below is from an SVM. -1 is not a fastball and 1 is a fastball", prediction)
+    )
+    
+    
+   
+  })
+  
+}
---
+++ shiny/ui.R
@@ -0,0 +1,27 @@
+ui <- pageWithSidebar(
+  
+  # App title ----
+  headerPanel(
+    "Baseball Pitch Prediction"
+  ),
+  
+  sidebarPanel(
+    p("Fill in the game scenario"),
+    numericInput("pitcher_id", "Pitcher id", 0, min = 0, step = 1),
+    numericInput("s_count", "Strike Count", 0, min = 0, max = 2, step = 1),
+    numericInput("b_count", "Ball Count", 0, min = 0, max = 3, step = 1),
+    textInput("last_ff_not", "Previous Pitch", value = "FF", width = NULL, placeholder = NULL),
+    numericInput("on_1b", "Runner on First Base(1 for yes/0 for no)", 0, min = 0, max = 1, step = 1),
+    numericInput("on_2b", "Runner on Second Base(1 for yes/0 for no)", 0, min = 0, max = 1, step = 1),
+    numericInput("on_3b", "Runner on Third Base(1 for yes/0 for no)", 0, min = 0, max = 1, step = 1),
+    textInput("stand", "Batter Stance(R/L)", value = "R", width = NULL, placeholder = NULL),
+    textInput("p_throws", "Pitcher Stance(R/L)", value = "R", width = NULL, placeholder = NULL),
+    
+    actionButton("go", "Click Here To Predict Next Pitch!!!"),
+  ),
+  
+  # Main panel for displaying outputs ----
+  mainPanel(
+    tableOutput(outputId = "table")
+  )
+)
